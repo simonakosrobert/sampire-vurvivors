cff0a4d5cdbcd53dc8dff693b1f36c7e71c87a42
simonakosrobert <126769620+simonakosrobert@users.noreply.github.com>
2023-09-20 22:36:25 +0200

    123

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pygame
 import math
+import random as rn
+from decimal import Decimal, getcontext
 
 #Local
 import settings
+import utilities
 
 pygame.init()
 
@@ -24,26 +27,28 @@ run = True
 background = pygame.image.load(r'images\background.jpg')
 background = pygame.transform.scale(background, (settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT))
 
+getcontext().prec = 3
 
 class Character():
-    def __init__(self, speed:int, pos:int, image_name:str, spawn_list:list, xp:int, point:int):
+    def __init__(self, speed:int, pos:int, image_name:str, spawn_list:list, xp:int, point:int, health: Decimal, dmg: Decimal):
         self.speed = speed
         self.spawn_list = spawn_list
         self.dict = {}
         self.xp = xp
         self.pos = pos
         self.point = point
+        self.max_health = Decimal(health)
+        self.health = Decimal(health)
+        self.dmg = Decimal(dmg)
         self.image = pygame.image.load(f'images\{image_name}.png').convert_alpha()
-        self.image = pygame.transform.scale(self.image, (settings.SCREEN_WIDTH/10, settings.SCREEN_HEIGHT/10))
+        self.image = pygame.transform.scale(self.image, (self.image.get_width()*((settings.SCREEN_HEIGHT+settings.SCREEN_WIDTH)/500), self.image.get_height()*((settings.SCREEN_HEIGHT+settings.SCREEN_WIDTH)/500)))
 
-panda = Character(5, [0, 0], 'panda', [], 0, 0)
+imre = Character(3, [0, 0], 'imre', [], 0, 0, 100, 0)
 #Panda initial starting point
-panda.pos = [(settings.SCREEN_WIDTH-panda.image.get_width())/2, (settings.SCREEN_HEIGHT-panda.image.get_height())/2]
+imre.pos = [(settings.SCREEN_WIDTH-imre.image.get_width())/2, (settings.SCREEN_HEIGHT-imre.image.get_height())/2]
 
-snow_leopard = Character(2, [0, 0], 'snow_leopard', [], 0, 0)
-snow_leopard.pos = [settings.SCREEN_WIDTH/4, settings.SCREEN_HEIGHT/4]
-
-
+elvira = Character(1, [0, 0], 'elvira', [], 0, 0, 50, 0.1)
+elvira.pos = [-40, rn.randint(50, 750)]
 
 def move_towards_player(self, player):
         # Find direction vector (dx, dy) between enemy and player.
@@ -59,26 +64,40 @@ if __name__ == '__main__':
 
     while run:
 
+        current_health = imre.health
+
         clock.tick(settings.FPS)
         tick += 1
         screen.fill(0)
         screen.blit(background, (0,0))
 
-        screen.blit(panda.image, panda.pos)
+        screen.blit(imre.image, imre.pos)
 
-        move_towards_player(snow_leopard, panda)
-        screen.blit(snow_leopard.image, snow_leopard.pos)
+        move_towards_player(elvira, imre)
+        screen.blit(elvira.image, elvira.pos)
+
+
+        if imre.pos[0] + 10 < elvira.pos[0] + elvira.image.get_width() and imre.pos[0] + imre.image.get_width() - 10 > elvira.pos[0] and imre.pos[1] + 10 < elvira.pos[1] + elvira.image.get_height() and imre.pos[1] + imre.image.get_height() - 10 > elvira.pos[1]:
+            imre.health -= elvira.dmg
+
+        health_percentage = float(imre.health) / float(imre.max_health)
+
+        print(health_percentage)
+
+
+        if imre.health < imre.max_health:
+            utilities.DrawBar(screen, (imre.pos[0], imre.pos[1] + imre.image.get_height() + 3), (40, 4), (0, 0, 0), (255, 0, 0), health_percentage)
 
         keys = pygame.key.get_pressed()
         
         if keys[pygame.K_s]:
-            panda.pos[1] += panda.speed
+            imre.pos[1] += imre.speed
         if keys[pygame.K_w]:
-            panda.pos[1] -= panda.speed
+            imre.pos[1] -= imre.speed
         if keys[pygame.K_d]:
-            panda.pos[0] += panda.speed
+            imre.pos[0] += imre.speed
         if keys[pygame.K_a]:
-            panda.pos[0] -= panda.speed
+            imre.pos[0] -= imre.speed
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
---
+++ utilities.py
@@ -0,0 +1,17 @@
+import pygame
+from decimal import Decimal, getcontext
+
+getcontext().prec = 3
+
+def DrawBar(screen, pos:tuple, size:tuple, borderC:tuple, barC:tuple, progress:float):
+
+    '''screen = screen to draw on\n
+    pos = Position of the bar\n
+    size = size of the bar\n
+    borderC = color of the border\n
+    barC = color of the bar'''
+
+    pygame.draw.rect(screen, borderC, (*pos, *size), 1)
+    innerPos  = (pos[0], pos[1])
+    innerSize = ((size[0]-1) * progress, size[1]-1)
+    pygame.draw.rect(screen, barC, (*innerPos, *innerSize))
